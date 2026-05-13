8b182e8fe87bf8f2712a55b7ad13acd4b744df41
Caca <ytchacagaming@gmail.com>
2026-05-13 10:00:39 +0700

    Add environment configuration and initial setup files for deployment

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 import pymysql
-import os
 
 app = Flask(__name__)
 
 def get_db():
     return pymysql.connect(
-        host=os.environ.get("MYSQLHOST"),
-        user=os.environ.get("MYSQLUSER"),
-        password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
-        port=int(os.environ.get("MYSQLPORT")),
+        host=os.environ.get("MYSQLHOST", "localhost"),
+        port=int(os.environ.get("MYSQLPORT", 3306)),
+        user=os.environ.get("MYSQLUSER", "root"),
+        password=os.environ.get("MYSQLPASSWORD", ""),
+        database=os.environ.get("MYSQLDATABASE", "buket_shop"),
         cursorclass=pymysql.cursors.DictCursor
     )
 
@@ -56,5 +56,4 @@ def kontak():
 
 # ================== RUN ==================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False)
